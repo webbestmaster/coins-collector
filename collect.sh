b3d0ef7a86f5b30498398a85cfe2ef695c2ba37e
Dmitry Turovtsov <web.best.master@gmail.com>
2016-11-09 11:59:36 +0300

    ...

--- collect.sh
+++ collect.sh
@@ -2,29 +2,48 @@
 
 #~/Android/Sdk/tools/emulator -avd Nexus_5_API_22 -wipe-data &
 
-cd ~/Android/Sdk/platform-tools/
+# set path to adb
+
+echo "email $1"
+echo "password $2"
+
+email=$1
+password=$2
+
+macOsAdbPath="/Library/Android/sdk/platform-tools/";
+# set right path fo linux
+linuxOsAdbPath="/Library/Android/sdk/platform-tools/";
+
+#pathToAdb;
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    pathToAdb=$linuxOsAdbPath;
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    pathToAdb=$macOsAdbPath;
+fi
+
+cd ~/$pathToAdb;
 
 # turn on device
 ./adb shell input keyevent KEYCODE_POWER
+echo "turn on";
 sleep 5s
 
 # unlock device
 ./adb shell input swipe 360 1000 630 1000 #Swipe RIGHT
+echo "unlock"
 sleep 5s
 
-#copy from device to desktop
-#./adb pull /sdcard/screen.png /home/dim/Desktop
-
-#sleep 20s
-#./adb wait-for-device &&
 ./adb shell pm clear com.alibaba.aliexpresshd
-#./adb uninstall ~/Downloads/app_5.0.1_apk-dl.com.apk
-#./adb install -r ~/Downloads/app_5.0.1_apk-dl.com.apk
-
+echo "app cleaded"
 sleep 5s
 
-#./adb shell input tap 905 575 # click to "git it" -> node notification
-#sleep 5s
+
+
+
+
+
+# actions
 
 ./adb shell input tap 360 1150 # click to bottom icon -> go apps
 sleep 5s
@@ -50,13 +69,13 @@ sleep 5s
 ./adb shell input tap 200 200 # click to e-mail
 sleep 2s
 
-./adb shell input text "lenard.rukol@gmail.com" # enter e-mail
+./adb shell input text $email # enter e-mail # "lenard.rukol@gmail.com"
 sleep 1s
 
 ./adb shell input tap 200 400 # click to password
 sleep 1s
 
-./adb shell input text "qwerty5678" # enter password
+./adb shell input text $password # enter password # "qwerty5678"
 sleep 1s
 
 ./adb shell input tap 320 500 # click to sign in
@@ -80,8 +99,16 @@ sleep 5s
 ./adb shell input tap 360 285 # click to coins
 sleep 5s
 
+
+
 ./adb shell screencap -p /sdcard/screen.png
-./adb pull /sdcard/screen.png /home/dim/Desktop
+./adb pull /sdcard/screen.png ~/Documents/
+echo "screen shot - done";
+sleep 3s
+
+./adb shell input keyevent KEYCODE_POWER
+echo "turn off";
+sleep 3s
 
 #
 #./adb shell input tap 160 220 # click to skip
