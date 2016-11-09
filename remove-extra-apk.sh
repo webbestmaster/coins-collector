1da149b529021cb9a8108e2cb9479b33fa95d579
Dmitry Turovtsov <web.best.master@gmail.com>
2016-11-09 16:48:00 +0300

    ...

---
+++ remove-extra-apk.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+cd ~/Library/Android/sdk/platform-tools/
+
+string=$(./adb shell 'pm list packages -f')
+
+#./adb uninstall
+
+#string="1:2:3:4:5"
+#set -f                      # avoid globbing (expansion of *).
+array=(${string//^/ ,g})
+item="";
+finded="data";
+appName="";
+for i in "${!array[@]}"
+do
+
+    item="${array[i]}";
+
+    if [[ "$item" == *"$finded"* ]]; then
+        appName=(${item//=/ });
+        appName="${appName[1]}";
+
+        echo "stop $appName"
+
+        aliExporess="com.alibaba.aliexpresshd"
+#        echo ${#aliExporess} # get length of string
+        appName="${appName%?}" # remove last extra character
+#        echo ${#appName}
+
+        if [[ "com.alibaba.aliexpresshd" != $appName ]]; then
+
+            echo "stop $appName"
+            ./adb shell am force-stop $appName
+            echo "clear $appName"
+            ./adb shell pm clear $appName
+            echo "uninstall $appName"
+            ./adb shell pm uninstall $appName
+            echo "-- uninstall $appName is done";
+
+        fi
+
+    fi
+
+done
+
